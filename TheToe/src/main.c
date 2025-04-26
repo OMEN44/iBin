999ddfe6e7be81fe152a5473f286559e7bc01480
Conner <connerjs345@gmail.com>
2025-04-26 13:56:14 +1000

    properly set up env

--- TheToe/src/main.c
+++ TheToe/src/main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
-#
+#include "driver/gpio.h"
+// #include "freeRTOS\freeRTOS.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+int counter = 0;
 
 void app_main() {
+
+
+    esp_rom_gpio_pad_select_gpio(8);
+    gpio_set_direction(8, GPIO_MODE_OUTPUT);
+
     while (1) {
-        printf("gaming\n");
+        // Blink the LED
+        gpio_set_level(8, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("ON\n");
+        gpio_set_level(8, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("OFF\n");
     }
 }
 
 
 
 
+/*
+#include <stdio.h>
 
-
+void app_main() {
+    while (1) {
+    printf("Ice cream\n");
+    }
+}
+*/
