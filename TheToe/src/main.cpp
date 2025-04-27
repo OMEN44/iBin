cfba268298caaceb497c5eb8ac11945ef2b6e6d6
Conner <connerjs345@gmail.com>
2025-04-27 14:21:42 +1000

    working head rotation and methods for waste and recycle

--- TheToe/src/main.cpp
+++ TheToe/src/main.cpp
@@ -1,22 +1,76 @@
-#include<Arduino.h>
-#include<stdio.h>
+#include <Arduino.h>
 #include <Servo.h>
 
-Servo servo;
+// Define servo objects
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+// Define pins
+#define SERVO1_PIN 2    // GP2
+#define SERVO2_PIN 3    // GP3
+#define SERVO3_PIN 4    // GP4
+#define BUTTON1_PIN 10  // GP10 (for Servo 1 and Servo 2 action)
+
+// Original servo angles
+#define SERVO1_ORIGINAL 93
+#define SERVO2_ORIGINAL 90
+#define SERVO3_ORIGINAL 90
+
+// Movement offsets
+#define SERVO1_OFFSET -3
+#define SERVO2_OFFSET 7
+#define SERVO3_LEFT 80     // Left position for Servo 3
+#define SERVO3_RIGHT 100   // Right position for Servo 3
+
+// Hold time in milliseconds
+#define HOLD_TIME 4000  // 2 seconds
+
+void moveServo3Left() {
+    servo3.write(SERVO3_LEFT);
+}
+
+void moveServo3Right() {
+    servo3.write(SERVO3_RIGHT);
+}
+
+void moveServo3Center() {
+    servo3.write(SERVO3_ORIGINAL);
+}
 
 void setup() {
-    servo.attach(4);
+    // Attach servos
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN);
+    servo3.attach(SERVO3_PIN);
+
+    // Move all servos to original positions
+    servo1.write(SERVO1_ORIGINAL);
+    servo2.write(SERVO2_ORIGINAL);
+    servo3.write(SERVO3_ORIGINAL);
+
+    // Setup button input with internal pull-up
+    pinMode(BUTTON1_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-    for (int i = 0; i < 90; i++) {
-        servo.write(i);
-        delay(50);
+    // Check if BUTTON1 is pressed (for Servo 1 and 2)
+    if (digitalRead(BUTTON1_PIN) == LOW) {
+        // Move Servo 1 and 2
+        servo1.write(SERVO1_ORIGINAL + SERVO1_OFFSET);
+        servo2.write(SERVO2_ORIGINAL + SERVO2_OFFSET);
+
+        delay(HOLD_TIME);
+
+        // Return to original positions
+        servo1.write(SERVO1_ORIGINAL);
+        servo2.write(SERVO2_ORIGINAL);
+
+        // Wait until button is released (debounce)
+        while (digitalRead(BUTTON1_PIN) == LOW) {
+            delay(10);
+        }
     }
-    for (int i = 90; i > 0; i--) {
-        servo.write(i);
-        delay(50);
-    }
-    
-    
+
+    // No code triggering Servo 3 yet
 }
